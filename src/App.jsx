a802a4cc2ed5e511529b7145dc98b9277c4bed6c
Hasangitt <pingpong038759@gmail.com>
2024-12-10 00:33:12 +0600

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,16 @@ function App() {
 
   return (
     <>
-      <div className='bg-black'>
+      <div className='starP bg-black'>
       <img src="https://upload.wikimedia.org/wikipedia/commons/2/26/You_Have_Been_Hacked!.jpg" alt="" />
       </div>
+      <div className='star'>
+       <p>****************************
+       **************************</p>
+       <p>**************************</p>
+      </div>
       <div>
-        <h1 className='text-green-500 text-xl font-bold mt-5'>If you want to recover your id, You have only 5 seconds.</h1>
+        <h1 className='text-green-500 text-xl font-bold mt-5'>If you want to r-e-c-o-v-e-r your i-d, You have only 5 seconds.</h1>
       </div>
       <div>
         <Link to="/haha"><button className='border px-4 py-2 rounded-lg mt-5 text-white font-semibold'>Click Here</button></Link>
--- src/Haha.jsx
+++ src/Haha.jsx
@@ -6,9 +6,9 @@ const Haha = () => {
            <img className="w-full" src="https://i.ibb.co.com/WWXLq07/11111111.jpg" alt="" />
            <div className="text-green-500">
             <h6>-----------------Note-------------------</h6>
-            <p>Hey! Just a heads-up: Always double-check links before opening them. That <span className="text-red-500">Win a Million Dollars</span> or <span className="text-red-500">Someones looking for you!</span> link could be a scam 🕵️‍♂️. Do not fall for shady stuff. <span className="text-white">stay smart, stay safe!</span></p>
+            <p>Hey! Always check when you are open an unknown link. <span className="text-white">stay smart, stay safe!</span></p>
            </div>
-           <p className="text-[10px] text-amber-100">Muhammad Hasan Ali Mijan</p>
+           <p className="text-[10px] text-amber-400">Muhammad Hasan Ali Mijan.........</p>
         </div>
     );
 };
